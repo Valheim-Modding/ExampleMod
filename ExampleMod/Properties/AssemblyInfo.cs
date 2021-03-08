add7ad37c1aa09cdfc44c38e2e38ff3188d4948b
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2021-03-08 22:58:02 +0100

    Update AssemblyTitle & AssemblyProduct

--- ExampleMod/Properties/AssemblyInfo.cs
+++ ExampleMod/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lead")]
+[assembly: AssemblyTitle("ExampleMod")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lead")]
+[assembly: AssemblyProduct("ExampleMod")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
